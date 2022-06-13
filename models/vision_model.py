1916227412e53e5adf4649c7002b659bf1b03f13
ziyuwwang <68987383+ziyuwwang@users.noreply.github.com>
2022-06-13 21:32:10 +0800

    Update vision_model.py

--- models/vision_model.py
+++ models/vision_model.py
@@ -19,11 +19,11 @@ def _cfg(url='', **kwargs):
 
 class VisionBlock(nn.Module):
 
-    def __init__(self, dim, resolution, num_head, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+    def __init__(self, dim, resolution, num_head, reduced_dim, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, skip_lam=1.0, mlp_fn=DynaMixerBlock):
         super().__init__()
         self.norm1 = norm_layer(dim)
-        self.attn = mlp_fn(dim, resolution=resolution, num_head=num_head, qkv_bias=qkv_bias, qk_scale=None,
+        self.attn = mlp_fn(dim, resolution=resolution, num_head=num_head, reduced_dim=reduced_dim, qkv_bias=qkv_bias, qk_scale=None,
                            attn_drop=attn_drop)
 
         # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
@@ -40,13 +40,13 @@ class VisionBlock(nn.Module):
         return x
 
 
-def basic_blocks(dim, index, layers, resolution, num_head, mlp_ratio=3., qkv_bias=False, qk_scale=None, \
+def basic_blocks(dim, index, layers, resolution, num_head, reduced_dim, mlp_ratio=3., qkv_bias=False, qk_scale=None, \
                  attn_drop=0, drop_path_rate=0., skip_lam=1.0, mlp_fn=DynaMixerBlock, **kwargs):
     blocks = []
 
     for block_idx in range(layers[index]):
         block_dpr = drop_path_rate * (block_idx + sum(layers[:index])) / (sum(layers) - 1)
-        blocks.append(VisionBlock(dim, resolution, num_head, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale, \
+        blocks.append(VisionBlock(dim, resolution, num_head, reduced_dim, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale, \
                                   attn_drop=attn_drop, drop_path=block_dpr, skip_lam=skip_lam, mlp_fn=mlp_fn))
 
     blocks = nn.Sequential(*blocks)
@@ -56,8 +56,8 @@ def basic_blocks(dim, index, layers, resolution, num_head, mlp_ratio=3., qkv_bia
 
 class VisionModel(nn.Module):
 
-    def __init__(self, layers, img_size=224, patch_size=4, in_chans=3, num_classes=1000,
-                 embed_dims=None, transitions=None, resolutions=None, num_heads=None, mlp_ratios=None, skip_lam=1.0,
+    def __init__(self, layers, img_size=224, patch_size=4, in_chans=3, num_classes=1000, embed_dims=None,
+                 transitions=None, resolutions=None, num_heads=None, reduced_dims=None, mlp_ratios=None, skip_lam=1.0,
                  qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0., drop_path_rate=0.,
                  norm_layer=nn.LayerNorm, mlp_fn=DynaMixerBlock, overlap=False):
 
@@ -69,8 +69,8 @@ class VisionModel(nn.Module):
 
         network = []
         for i in range(len(layers)):
-            stage = basic_blocks(embed_dims[i], i, layers, resolutions[i], num_heads[i], mlp_ratio=mlp_ratios[i],
-                                 qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop_rate,
+            stage = basic_blocks(embed_dims[i], i, layers, resolutions[i], num_heads[i], reduced_dims[i],
+                                 mlp_ratio=mlp_ratios[i], qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop_rate,
                                  drop_path_rate=drop_path_rate, norm_layer=norm_layer, skip_lam=skip_lam, mlp_fn=mlp_fn)
             network.append(stage)
             if i >= len(layers) - 1:
